5e2e9bcfe8f212d4cb6442414b2e147458911266
ivicakartelo <ivicakartelo@users.noreply.github.com>
2023-03-03 11:29:38 +0100

    every raw is paragraph

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,32 +1,36 @@
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function BlogPost() {
+  const [APIData, setAPIData] = useState([]);
 
-    const [APIData, setAPIData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`https://640114a00a2a1afebee5c77d.mockapi.io/post`)
+      .then((response) => {
+        setAPIData(response.data);
+      });
+  }, []);
 
-    useEffect(() => {
-        axios.get(`https://640114a00a2a1afebee5c77d.mockapi.io/post`)
-            .then((response) => {
-                setAPIData(response.data);
-            })
-    }, [])
+  const params = useParams();
+  const id = params.id;
+  const blogpost = APIData.find((post) => post.id === id);
 
-    const params = useParams()
-    const id = params.id
-    const blogpost = APIData.find(post => post.id === id)
+  if (!blogpost) {
+    return <p>Blog post not found.</p>;
+  }
 
-    if (!blogpost) {
-        return <p>Blog post not found.</p>;
-    }
+  const paragraphs = blogpost.blogpost.split('\n').map((paragraph, index) => (
+    <p key={index}>{paragraph}</p>
+  ));
 
-    return (
-        <>
-            <h1>{blogpost.heading}</h1>
-            <p>{blogpost.blogpost}</p>
-        </>
-    )		
+  return (
+    <>
+      <h1>{blogpost.heading}</h1>
+      {paragraphs}
+    </>
+  );
 }
 
 export default BlogPost;
